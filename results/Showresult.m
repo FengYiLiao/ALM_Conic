c69a703522e35dfc85271d0a3c58322274be7549
Feng-Yi Liao <86756536+FengYiLiao@users.noreply.github.com>
2024-05-19 10:06:13 -0700

    Add files via upload

--- results/Showresult.m
+++ results/Showresult.m
@@ -30,46 +30,16 @@ for idx = 1:3
     Feasibility = max([Out.Affinefeasi;Out.DAffinefeasi;Out.Conefeasi]);
     semilogy(Feasibility(range),'-o');
     semilogy(Out.DCostgap(range),'-o');
-%     if idx == 1
-%         semilogy(Out.Dist,'-o');
-%         hold on 
-%         Feasibility = max([Out.Affinefeasi;Out.DAffinefeasi;Out.Conefeasi]);
-%         semilogy(Feasibility,'-o');
-%         semilogy(Out.DCostgap,'-o');
-%     else
-%         semilogy(Out.Dist,'-o');
-%         hold on 
-%         Feasibility = max([Out.Affinefeasi;Out.DAffinefeasi;Out.Conefeasi]);
-%         semilogy(Feasibility,'-o');
-%         semilogy(Out.DCostgap,'-o');
-%     end
-       
-
-    %semilogy(Out.Dist,'-o');
-    %hold on 
-    %semilogy(Out.DDist,'-o');
-    %semilogy(Out.PCostgap,'-o');
-    %semilogy(Out.DCostgap,'-o');
-    %Feasibility = max([Out.Affinefeasi;Out.DAffinefeasi;Out.Conefeasi]);
-    %semilogy(Feasibility,'-o','Color',colors{4});
     xlabel('Iteration','interpreter','latex');
     xlim([range(1),range(end)]);
-    %legeng('')
-    %ylabel('$(f(x_k) - f^\star)/f^\star $','interpreter','latex');
     set(gca,'TickLabelInterpreter','latex' ,'FontSize', fsz, 'LineWidth', alw); %<- Set properties
     if idx == 1
         set(gcf, 'Position', [300 100  width*100, height*100]); %<- Set size
         set(gca, 'Position', [0.1 0.3 0.225 0.6]); %<- Set properties
         title('Max-Cut','FontSize', fsz);
-%         legend('$\frac{\mathrm{Dist}(X_k,\Omega_{\mathrm{P}})}{1+\|X^\star\|}$',...
-%     '$\frac{\mathrm{Dist}((y_k,Z_k),\Omega_{\mathrm{D}})}{1+\|y^\star\|+\|Z^\star\|}$',...
-%     'interpreter','latex','FontSize', fsz);%,'Position',[0.115,0.03,0.8,0.1]
     elseif idx == 2
         set(gca, 'Position', [0.405 0.3 0.225 0.6]); %<- Set properties
         title('Matrix-Completion','FontSize', fsz);
-%         legend('$\frac{\mathrm{Dist}(X_k,\Omega_{\mathrm{P}})}{1+\|X^\star\|}$',...
-%     '$\frac{\mathrm{Dist}((y_k,Z_k),\Omega_{\mathrm{D}})}{1+\|y^\star\|+\|Z^\star\|}$',...
-%     'interpreter','latex','FontSize', fsz);
     elseif idx == 3
         set(gca, 'Position', [0.71 0.3 0.225 0.6]); %<- Set properties
         title('BQP','FontSize', fsz);
@@ -79,10 +49,6 @@ lg = legend( '$\frac{\mathrm{Dist}(X_k,\Omega_{\mathrm{P}})}{1+\|X^\star\|}$',..
     '$\left |\frac{\langle b,y_k \rangle -d^\star }{d^\star}\right|$', ...
     '$\max\left \{\frac{\|\mathcal{A}(X_k)-b\|}{1+\|b\|},\frac{\|X_k - \Pi_{\mathcal{K}}(X_k)\|}{1+\|X_k\|},\frac{\|C-\mathcal{A}^*(y_k)-Z_k\|}{1+\|C\|},\frac{\|Z_k - \Pi_{\mathcal{K}^*}(Z_k)\|}{1+\|Z_k\|} \right\}$',...
         'interpreter','latex','NumColumns',4,'Position',[0.115,0.03,0.8,0.1],'FontSize', fsz);
-% '$\left |\frac{\langle C,X_k \rangle - p^\star }{p^\star}\right|$',...    
-% '$\left |\frac{\langle b,y_k \rangle -d^\star }{d^\star}\right|$',...
-        %'$\max\left \{\frac{\|\mathcal{A}(X_k)-b\|}{1+\|b\|},\frac{\|X_k - \Pi_{\mathcal{K}}(X_k)\|}{1+\|X_k\|},\frac{\|C-\mathcal{A}^*(y_k)-Z_k\|}{1+\|C\|},\frac{\|Z_k - \Pi_{\mathcal{K}^*}(Z_k)\|}{1+\|Z_k\|} \right\}$',
-        %'$\frac{\mathrm{Dist}((y_k,Z_k),\Omega_{\mathrm{D}})}{1+\|y^\star\|+\|Z^\star\|}$',...
 
 legend('boxoff');
 

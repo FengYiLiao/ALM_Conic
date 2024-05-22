e78c067d52d62b842a3e751dae5b21f18d4d75bc
Feng-Yi Liao <86756536+FengYiLiao@users.noreply.github.com>
2024-05-21 17:58:35 -0700

    Add files via upload

--- results/Showresult.m
+++ results/Showresult.m
@@ -3,7 +3,7 @@ close all;clc; clear;
 %description
 
 %parameter setting 
-width  = 9;     % Width in inches
+width  = 10;     % Width in inches
 height = 3;    % Height in inches
 alw    = 0.75;    % AxesLineWidth
 fsz    = 12;      % Fontsize
@@ -23,32 +23,61 @@ name     = {'G1_n20','n20r5MC','BQP_22'};
 range = 1:6;
 
 for idx = 1:3
-    subplot(1,3,idx);
-    load(datapath+name{idx}+"_result"); 
+    load(datapath+name{idx}+"_result");
+    subplot(1,3,1);
     semilogy(Out.Dist(range),'-o');
     hold on 
-    Feasibility = max([Out.Affinefeasi;Out.DAffinefeasi;Out.Conefeasi]);
+    %
+    Feasibility = max([Out.Affinefeasi;Out.DAffinefeasi;Out.Conefeasi;abs(Out.PCost - Out.DCost)/(1+abs(Out.OptimalCost))]);
+    subplot(1,3,2);
     semilogy(Feasibility(range),'-o');
+    hold on 
+
+    subplot(1,3,3);
     semilogy(Out.DCostgap(range),'-o');
-    xlabel('Iteration','interpreter','latex');
-    xlim([range(1),range(end)]);
-    set(gca,'TickLabelInterpreter','latex' ,'FontSize', fsz, 'LineWidth', alw); %<- Set properties
-    if idx == 1
-        set(gcf, 'Position', [300 100  width*100, height*100]); %<- Set size
-        set(gca, 'Position', [0.1 0.3 0.225 0.6]); %<- Set properties
-        title('Max-Cut','FontSize', fsz);
-    elseif idx == 2
-        set(gca, 'Position', [0.405 0.3 0.225 0.6]); %<- Set properties
-        title('Matrix-Completion','FontSize', fsz);
-    elseif idx == 3
-        set(gca, 'Position', [0.71 0.3 0.225 0.6]); %<- Set properties
-        title('BQP','FontSize', fsz);
-    end
+    hold on 
+
+    
 end
-lg = legend( '$\frac{\mathrm{Dist}(X_k,\Omega_{\mathrm{P}})}{1+\|X^\star\|}$',...
-    '$\left |\frac{\langle b,y_k \rangle -d^\star }{d^\star}\right|$', ...
-    '$\max\left \{\frac{\|\mathcal{A}(X_k)-b\|}{1+\|b\|},\frac{\|X_k - \Pi_{\mathcal{K}}(X_k)\|}{1+\|X_k\|},\frac{\|C-\mathcal{A}^*(y_k)-Z_k\|}{1+\|C\|},\frac{\|Z_k - \Pi_{\mathcal{K}^*}(Z_k)\|}{1+\|Z_k\|} \right\}$',...
-        'interpreter','latex','NumColumns',4,'Position',[0.115,0.03,0.8,0.1],'FontSize', fsz);
+
+set(gcf, 'Position', [300 100  width*100, height*100]); %<- Set size
+
+subplot(1,3,1);
+xlim([range(1),range(end)]);
+xlabel('Iteration','interpreter','latex');
+set(gca,'TickLabelInterpreter','latex' ,'FontSize', fsz, 'LineWidth', alw); %<- Set properties
+ylabel('$\log_{10}\left(\frac{\mathrm{Dist}(X_k,\Omega_{\mathrm{P}})}{1+\|X^\star\|}\right)$','interpreter','latex','FontSize', 14);
+set(gca, 'Position', [0.1 0.3 0.225 0.6]); %<- Set properties
+title('Primal distance','interpreter','latex','FontSize', fsz);
+hold on 
+
+subplot(1,3,2);
+xlim([range(1),range(end)]);
+xlabel('Iteration','interpreter','latex');
+set(gca,'TickLabelInterpreter','latex' ,'FontSize', fsz, 'LineWidth', alw); %<- Set properties
+ylabel('$\log_{10}\left ( \frac{ | \langle b,y_k \rangle -d^\star | }{| d^\star |} \right )$','interpreter','latex','FontSize', 16);
+set(gca, 'Position', [0.42 0.3 0.225 0.6]); %<- Set properties
+title('Dual cost value gap','interpreter','latex','FontSize', fsz);
+hold on 
+
+
+subplot(1,3,3);
+xlim([range(1),range(end)]);
+xlabel('Iteration','interpreter','latex');
+set(gca,'TickLabelInterpreter','latex' ,'FontSize', fsz, 'LineWidth', alw); %<- Set properties
+ylabel('$\log_{10}\left ( e \right )$','interpreter','latex', 'LineWidth', alw,'FontSize', 14)
+set(gca, 'Position', [0.73 0.3 0.225 0.6]); %<- Set properties
+title('KKT Residuals','interpreter','latex','FontSize', fsz);
+hold on 
+    
+
+% lg = legend( '$\frac{\mathrm{Dist}(X_k,\Omega_{\mathrm{P}})}{1+\|X^\star\|}$',...
+%     '$\left |\frac{\langle b,y_k \rangle -d^\star }{d^\star}\right|$', ...
+%     '$\max\left \{\frac{\|\mathcal{A}(X_k)-b\|}{1+\|b\|},\frac{\|X_k - \Pi_{\mathcal{K}}(X_k)\|}{1+\|X_k\|},\frac{\|C-\mathcal{A}^*(y_k)-Z_k\|}{1+\|C\|},\frac{\|Z_k - \Pi_{\mathcal{K}^*}(Z_k)\|}{1+\|Z_k\|} \right\}$',...
+%         'interpreter','latex','NumColumns',4,'Position',[0.115,0.03,0.8,0.1],'FontSize', fsz);
+lg = legend( 'Max-Cut',...
+    'Matrix-Completion', ...
+    'Binary quadratic program','interpreter','latex','NumColumns',4,'Position',[0.115,0.03,0.8,0.1],'FontSize', fsz);
 
 legend('boxoff');
 
